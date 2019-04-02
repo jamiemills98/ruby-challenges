5fbf35d2c75ecb4a4e80e49d5d74268e67a723db
jamiemills98 <jamiemills98@hotmail.com>
2019-04-03 09:50:44 +1000

    edit

---
+++ 03_largest_number.rb
@@ -0,0 +1,24 @@
+
+
+  # Your code here
+
+  largest_number = []
+
+  puts "Type in any number"
+    number_1 = gets.to_i
+      largest_number << number_1
+
+  puts "Type in any number"
+    number_2 = gets.to_i
+      largest_number << number_2
+
+  puts "The largest number you entered is: "
+  puts largest_number.max
+
+
+
+
+
+
+
+  
