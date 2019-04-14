73ca57ab2c5e781292a856aea4ad6b3b77f26274
jamiemills98 <jamiemills98@hotmail.com>
2019-04-14 14:14:30 +1000

    touch ups

---
+++ cafeProfit.rb
@@ -0,0 +1,21 @@
+class Profit
+
+    def profits(num1, num2) 
+        return num1 - num2
+    end
+
+end
+
+latte = Profit.new
+latte.profits(4, 2) * add_order[0][0]
+puts "$#{latte.profits(4, 2) * add_order[0][0]} profit"
+
+scones = Profit.new
+scones.profits(5, 3)
+puts "$#{scones.profits(5, 3) * add_order[1][0]} profit"
+
+tea = Profit.new
+tea.profits(3, 0.5)
+puts "$#{tea.profits(3, 0.5 ) * add_order[2][0]} profit"
+
+
