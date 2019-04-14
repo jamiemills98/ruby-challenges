069d5df9ae43dd93a8f05a097ab8d19aee0a07b5
jamiemills98 <jamiemills98@hotmail.com>
2019-04-14 13:57:07 +1000

    order $ amount

--- cafepart3.rb
+++ cafepart3.rb
@@ -7,9 +7,9 @@
 
 
 add_order = []
-add_order[0] = [0, "Lattes"]
-add_order[1] = [0, "Scones"]
-add_order[2] = [0, "Teas"]
+add_order[0] = [0, "Lattes", 0]
+add_order[1] = [0, "Scones", 0]
+add_order[2] = [0, "Teas", 0]
 
 
 loop do
@@ -19,20 +19,22 @@ puts "What would you like to order (latte, scone, tea)? Type (q)uit to quit."
     case customer
     when "latte", "Latte"
         puts "How many?"
-        latte_amount = gets.to_i
-        add_order[0][0] += latte_amount
+        answer = gets.to_i
+        add_order[0][0] += answer
+        add_order[0][2] += answer * 4
         
 
     when "scone", "Scone"
         puts "How many?"
         answer = gets.to_i
         add_order[1][0] += answer
+        add_order[1][2] += answer * 5
         
     when "tea", "Tea"
         puts "How many?"
         answer = gets.to_i
         add_order[2][0] += answer
-        
+        add_order[2][2] += answer * 3
 
     when "q", "Q"
         break
@@ -47,8 +49,14 @@ puts "Thank you! here is your order: "
 add_order.each do |key, value|
     puts "#{key} #{value}"
 end
-puts "Total cost = $ "
+amount = []
+amount << add_order[0][2]
+amount << add_order[1][2]
+amount << add_order[2][2]
 
+amount.each do |value|
+puts "Total order= $ #{value}"
+end
 
 # latte
 # How many?
