3155e6d273fe34692ea5658070d64c3923691fd9
jamiemills98 <jamiemills98@hotmail.com>
2019-05-08 08:43:40 +1000

    testing

--- tests/16_palindrome_test.rb
+++ tests/16_palindrome_test.rb
@@ -1,1 +1,10 @@
-# Add tests to me :D
+require 'test/unit'
+require_relative '../16_palindrome'
+
+class NthFibonacciTest < Test::Unit::TestCase
+  def test_palindrome
+    assert_equal(true, palindrome?("mom"))
+    assert_equal(true, palindrome?("racecar"))
+    assert_equal(false, palindrome?("good"))
+  end
+end
